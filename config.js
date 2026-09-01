8dd3a597dccaaddf10132398621995481e93d5c8
jmprogdev <clapyyyyy07@gmail.com>
2026-09-01 22:40:15 +0800

    Update VIGAN_SHARED_API URL in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
    After deploying backend/worker.js, paste the Worker URL below.
    Example: https://vigan-wonder-tour-api.yourname.workers.dev
 */
-window.VIGAN_SHARED_API = " https://vigan-wonder-tour-api.31313123135453.workers.dev";
+window.VIGAN_SHARED_API = "https://vigan-wonder-tour-api.jmprogdev404error.workers.dev";
